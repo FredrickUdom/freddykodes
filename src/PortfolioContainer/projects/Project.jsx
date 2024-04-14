d8616b5950260242282afa7e6632d5d3fa6960d6
fredrickUdom <fredrickudom@gmail.com>
2024-04-14 16:11:35 +0100

    fxing a project section

--- src/PortfolioContainer/projects/Project.jsx
+++ src/PortfolioContainer/projects/Project.jsx
@@ -1,12 +1,38 @@
+// import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+// import { Carousel } from 'react-responsive-carousel'; 
+import laptopSystem from '../../assets/img/system.jpg';
 const Project = () => {
-    return ( 
-        <div id="project-section">
+  return (
+    <div id="project-section">
 
-
-          
+      <div class="content col-md-6">
+        <div class="shadow px-3 about bg-primary text-white certificate">
+          {/* <h3><b>CERTIFICATES</b></h3> */}
+          {/* <Carousel> */}
+          <div>
+            <img src={laptopSystem} style={{ width: "30%" }} />
+            <h1 className="legend text-bold-500">Web Design Skills</h1>
+            <p>I create visually appealing web designs that suit your needs!
+              <h1 className="legend text-bold-500">Graphic Design</h1>
+            </p>
+          </div>
+          <div>
+            <img src={laptopSystem} style={{ width: "30%" }} />
+            <h1 className="legend">Software Development</h1>
+            <p>My digital skills enable me to create functional and visually appealing UI and applications. I deliver exceptional results and drive growth that meet the unique needs of my clients and employers</p>
+          </div>
+          <div>
+            <img src={laptopSystem} style={{ width: "30%" }} />
+            <h1 className="legend font-bold">Entrepreneurship </h1>
+            <p>I leverage my digital skills in a customer-centric and result-driven way!</p>
+          </div>
+          {/* </Carousel> */}
         </div>
+      </div>
 
-     );
+    </div>
+
+  );
 }
- 
+
 export default Project;
