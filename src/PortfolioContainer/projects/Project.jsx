cfcc3354ac83911c292fcb82104417c6ec227952
fredrickUdom <fredrickudom@gmail.com>
2024-04-14 16:09:34 +0100

    creating a project section

--- src/PortfolioContainer/projects/Project.jsx
+++ src/PortfolioContainer/projects/Project.jsx
@@ -1,48 +1,10 @@
 const Project = () => {
     return ( 
-        <section class="ftco-section" id="services-section">
-  <div class="container">
-    <div class="row justify-content-center py-5 mt-5">
-      <div class="col-md-12 heading-section text-center ftco-animate">
-        <h2 class="big big-2"><p>PROJECTS</p></h2>
-        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
-      </div>
-    </div>
+        <div id="project-section">
 
-    <div class="row">
-      <div class="col-lg-4 col-md-6 text-center d-flex ftco-animate shadow">
-        <a href="#" class="project-1 w-100">
-          <span class="icon">
-            <img src={nestjs} alt="" class="img-fluid"/>
-          </span>
-          <div class="desc">
-            <h3>I write Nestjs for building scalable server-side APIs</h3>
-          </div>
-        </a>
-      </div>
-      <div class="col-lg-4 col-md-6 text-center d-flex ftco-animate shadow">
-        <a href="#" class="project-1 w-100">
-          <span class="icon">
-            <img src={typescript} alt="" class="img-fluid"/>
-          </span>
-          <div class="desc">
-            <h3>I use Typescript and Nodejs with Nestjs</h3>
-          </div>
-        </a>
-      </div>
-      <div class="col-lg-4 col-md-6 text-center d-flex ftco-animate shadow">
-        <a href="#" class="project-1 w-100">
-          <span class="icon">
-            <img src={docker} alt="" class="img-fluid"/>
-          </span>
-          <div class="desc">
-            <h3>Using Docker for containerization</h3>
-          </div>
-        </a>
-      </div>
-    </div>
-  </div>
-</section>
+
+          
+        </div>
 
      );
 }
