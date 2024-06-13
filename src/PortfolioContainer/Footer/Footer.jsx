c53adc69851001defa8272a80918c145fd176307
fredrickUdom <fredrickudom@gmail.com>
2024-06-13 16:35:13 +0100

    fixing footer

--- src/PortfolioContainer/Footer/Footer.jsx
+++ src/PortfolioContainer/Footer/Footer.jsx
@@ -1,13 +1,46 @@
 import '../Footer/Footer.css';
+
 const FooterArea = () => {
+    const date = new Date().getFullYear()
     return ( 
         <>
-        <div className="footer">
-            
-            {/* <marquee behavior="" direction="">welcome to my Freddy Kodes</marquee> */}
+        <div className="footer text-white text-center py-2">
+        <div className="colz-icon">
 
-            <p>welcome</p>
-        </div>
+                           
+                            <a href="https://www.facebook.com/freddyfred93">
+                                <i className="fa fa-facebook-square px-4" aria-hidden="true"></i>
+                            </a>
+                            <a href="">
+                                <i className="fa fa-whatsapp" aria-hidden="true"></i>
+                            </a>
+                            {/* <a href="">
+                                <i className="fa fa-google-plus-square" aria-hidden="true"></i>
+                            </a> */}
+                            <a href="https://www.instagram.com/fredrickudom/">
+                                <i className="fa fa-instagram px-4" aria-hidden="true"></i>
+                            </a>
+                            {/* <a href="">
+                                <i className="fa fa-youtube-square" aria-hidden="true"></i>
+                            </a> */}
+                            <a href="https://x.com/FredrickUdom">
+                                <i className="fa fa-twitter px-4" aria-hidden="true"></i>
+                            </a>
+                            
+                            <a href="https://github.com/FredrickUdom">
+                            <i class="fa fa-github" aria-hidden="true"></i>
+                            </a>
+                            <a href="https://www.linkedin.com/in/fredrick-udom-696085227/">
+                                <i class="fa fa-linkedin px-4" aria-hidden="true"></i>
+                                
+                            </a>
+                        </div>
+                        <div className="copy-right pt-3">
+                        <p>&copy; {date} FreddyKodes</p>
+                        </div>
+                    </div>
+            {/* <marquee behavior="" direction="">welcome to my Freddy Kodes</marquee> */}
+        
         </>
      );
 }
--- src/PortfolioContainer/Home/Profile.jsx
+++ src/PortfolioContainer/Home/Profile.jsx
@@ -20,8 +20,8 @@ function Profile() {
                         <div className="colz-icon">
 
                            
-                            <a href="https://www.facebook.com/freddyfred93">
-                                <i className="fa fa-facebook-square" aria-hidden="true"></i>
+                        <a href="https://www.facebook.com/freddyfred93">
+                                <i className="fa fa-facebook-square px-4" aria-hidden="true"></i>
                             </a>
                             <a href="">
                                 <i className="fa fa-whatsapp" aria-hidden="true"></i>
@@ -30,18 +30,21 @@ function Profile() {
                                 <i className="fa fa-google-plus-square" aria-hidden="true"></i>
                             </a> */}
                             <a href="https://www.instagram.com/fredrickudom/">
-                                <i className="fa fa-instagram" aria-hidden="true"></i>
+                                <i className="fa fa-instagram px-4" aria-hidden="true"></i>
                             </a>
                             {/* <a href="">
                                 <i className="fa fa-youtube-square" aria-hidden="true"></i>
                             </a> */}
                             <a href="https://x.com/FredrickUdom">
-                                <i className="fa fa-twitter" aria-hidden="true"></i>
+                                <i className="fa fa-twitter px-4" aria-hidden="true"></i>
                             </a>
                             
                             <a href="https://github.com/FredrickUdom">
-                                <i class="fa fa-linkedin" aria-hidden="true"></i>
-                                <i class="fa fa-github" aria-hidden="true"></i>
+                            <i class="fa fa-github" aria-hidden="true"></i>
+                            </a>
+                            <a href="https://www.linkedin.com/in/fredrick-udom-696085227/">
+                                <i class="fa fa-linkedin px-4" aria-hidden="true"></i>
+                                
                             </a>
                         </div>
 
